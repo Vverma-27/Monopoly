f94b20d8faf9aab2ce8dc2160c315bcd695817e5
Vihaan <vverma270705@gmail.com>
2024-10-11 15:47:21 +0530

    notifications was hidden

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -146,7 +146,7 @@ const Notifications = () => {
   };
 
   return (
-    <div className="fixed top-5 xs:right-20 right-24">
+    <div className="fixed top-5 xs:right-20 right-24 xl:right-64">
       {offerNotifs > 0 && (
         <div className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
           {offerNotifs}
